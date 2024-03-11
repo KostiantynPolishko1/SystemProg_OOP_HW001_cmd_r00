834e8af3e462fddd33fdfd86e0f42d776fc1900e
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2024-03-11 17:19:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SystemProg_OOP_HW001_cmd_r00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
