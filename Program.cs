1294bf598c4e68b3f110f615b702896ae24d1df7
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2024-03-11 23:05:11 +0200

    2. Added HashSet<(string, int)> in order to have collection of unique objects as tuple (string ProcessName, int BasePriority).

--- Program.cs
+++ Program.cs
@@ -6,38 +6,68 @@ namespace SystemProg_OOP_HW001_cmd_r00
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.Write("\tname of App: ");
-                string? appName = Console.ReadLine();
+            /*            try
+                        {
+                            Console.Write("\tname of App: ");
+                            string? appName = Console.ReadLine();
 
-                if (appName == null || appName == string.Empty)
-                {
-                    throw new Exception($"No enter name off App");
-                }
-                else
-                {
-                    try
-                    {
-                        StatusApp(IsLaunchApp(Process.GetProcesses().ToList(), appName));
-                    }
-                    catch (InvalidOperationException ex)
-                    {
-                        ShowMsgExp($"\tError!\n\t{appName} does not have an identifier.");
-                    }
-                    catch (NotSupportedException ex)
-                    {
-                        ShowMsgExp($"\tError!\n\t{appName} is not on this computer.");
-                    }
-                    catch (Exception ex)
-                    {
-                        ShowMsgExp(ex.Message);
-                    }
-                }
-            }
-            catch (Exception MyEx)
+                            if (appName == null || appName == string.Empty)
+                            {
+                                throw new Exception($"No enter name off App");
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    StatusApp(IsLaunchApp(Process.GetProcesses().ToList(), appName));
+                                }
+                                catch (InvalidOperationException ex)
+                                {
+                                    ShowMsgExp($"\tError!\n\t{appName} does not have an identifier.");
+                                }
+                                catch (NotSupportedException ex)
+                                {
+                                    ShowMsgExp($"\tError!\n\t{appName} is not on this computer.");
+                                }
+                                catch (Exception ex)
+                                {
+                                    ShowMsgExp(ex.Message);
+                                }
+                            }
+                        }
+                        catch (Exception MyEx)
+                        {
+                            ShowMsgExp(MyEx.Message);
+                        }*/
+
+
+
+            //ShowListApp(Process.GetProcesses().ToList());
+
+            HashSet<(string, int)> tmp = GetNamesPriorities(Process.GetProcesses().ToList());
+            ShowListApp(tmp, 7);
+
+            HashSet<(string, int)> GetNamesPriorities(in List<Process> processs)
             {
-                ShowMsgExp(MyEx.Message);
+                HashSet<(string, int)> NamesPriorities = new HashSet<(string, int)>() { };
+                foreach (Process p in processs)
+                {
+                    NamesPriorities.Add((p.ProcessName, p.BasePriority));
+                }
+
+                return NamesPriorities;
+            }
+
+            void ShowListApp(in HashSet<(string, int)> process, int index = 0)
+            {
+                int i = 0;
+                foreach ((string name, int priority) item in process)
+                {
+                    if(item.priority > index)
+                    {
+                        Console.WriteLine($"{i += 1}. {item.name} - {item.priority}");
+                    }
+                }
             }
 
             void ShowMsgExp(in string msg)
